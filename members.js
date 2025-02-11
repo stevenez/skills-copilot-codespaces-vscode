c4026f40e421180c070c5a1590fc661457832364
stevenez <stevenez95@gmail.com>
2025-02-11 19:12:11 +0000

    second

---
+++ members.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        address: {
+            city: 'Boston',
+            state: 'MA'
+        }
+    };
+    return member;
+}
